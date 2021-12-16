4f0f90b84cd8bb4effc1a2c84abcf6a419b94ae2
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-12-16 17:20:04 +0000

    Question 2 compile code added

--- Report/Question 2/Question2/Question2/kernel.cu
+++ Report/Question 2/Question2/Question2/kernel.cu
@@ -5,8 +5,8 @@
 ------------------UNIVERSITY OF PLYMOUTH, SCHOOL OF ENGINEERING, COMPUTING AND MATHEMATICS---
 */
 
-#include <cuda.h> 
-#include <cuda_runtime.h> 
+#include <cuda.h>
+#include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,14 +14,11 @@
 #include <math.h>
 #include <omp.h>
 
-#define TIMES_TO_RUN 10 //how many times the function will run
+#define TIMES_TO_RUN 1 //how many times the function will run
 
-#define N 128 //input size - USE POWER OF 2 ONLY 
-#define CHECK_OUTPUT   //if do not want to validate the results comment this 
+#define N 256 //input size - USE POWER OF 2 ONLY
+//#define CHECK_OUTPUT   //if do not want to validate the results comment this
 
-#define TILE 32 //use either 32 or 16 only
-#define TILE_x2 TILE*2
-#define TILE_x4 TILE*4
 
 __declspec(align(64)) float C[N * N], test[N * N], A[N * N], B[N * N]; //square matrixes are considered only, stored as 1d arrays
 
@@ -36,45 +33,23 @@ inline unsigned short int equal(float const a, float const b);
 #define MAX_NUMBER_OF_BLOCKS_PER_DIM 65535 //max number of blocks that our GPU can handle (for one dimension only)
 
 
-__global__ void mmm_implementation1(float* C, float* A, float* B) {
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			for (int k = 0; k < N; k++)
-				C[N * i + j] += A[N * i + k] * B[N * k + j];
-}
 
-__global__ void mmm_implementation2() {
-	__declspec(align(64)) float C[N * N], A[N * N], B[N * N];
-#pragma omp parallel  
-	{
-#pragma omp for private(i, j, k, tmp) 
+__global__ void mmm_ver1(float* C, float* A, float* B) {
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				float tmp = 0.0;
+	//#Implementation #3
 
-#pragma omp simd reduction(+:tmp) aligned(C,A,B:64) 
+	//use dim3 dimBlock(16, 16, 1);
+	//use dim3 dimGrid(N/16, N/16, 1);
 
-				for (int k = 0; k < N; k++) {
-					tmp += A[N * i + k] * B[N * k + j];
-				}
-				C[N * i + j] = tmp;
-			}
-		}
-	}
-}
-
-//#Implementation #3 
-
-//use dim3 dimBlock(16, 16, 1); 
-//use dim3 dimGrid(N/16, N/16, 1); 
-
-__global__ void mmm_implementation3(float* C, float* A, float* B) {
 	float tmp = 0.0;
 
-	int i = blockIdx.x * blockDim.x + threadIdx.x; //i loop has been parallelized 
 
-	int j = blockIdx.y * blockDim.y + threadIdx.y; //j loop has been parallelized 
+
+	int i = blockIdx.x * blockDim.x + threadIdx.x; //i loop has been parallelized
+
+	int j = blockIdx.y * blockDim.y + threadIdx.y; //j loop has been parallelized
+
+
 
 	for (int k = 0; k < N; k++) {
 
@@ -82,26 +57,36 @@ __global__ void mmm_implementation3(float* C, float* A, float* B) {
 
 	}
 
+
+
 	C[N * i + j] = tmp;
 }
 
-//implementation #4 
-//use dim3 dimBlock(16, 16, 1); 
-//use dim3 dimGrid(N/16, N/16, 1); 
+//implementation #4
+//use dim3 dimBlock(16, 16, 1);
+//use dim3 dimGrid(N/16, N/16, 1);
 
-__global__ void mmm_implementation4(float* C, float* A, float* B) {
+__global__ void mmm_tiled(float* C, float* A, float* B) {
 	__shared__ float aa[16][16];
+
 	__shared__ float bb[16][16];
 
 	float tmp = 0.0;
+
 	int k, m;
 
+
+
 	int row_A = 16 * blockIdx.y + threadIdx.y;
+
 	int col_B = blockIdx.x * 16 + threadIdx.x;
 
+
+
 	for (m = 0; m < N / 16; m++) {
+
 		aa[threadIdx.y][threadIdx.x] = A[N * (row_A)+(m * 16 + threadIdx.x)];
-		bb[threadIdx.y][threadIdx.x] = B[N * (m * TILE + threadIdx.y) + (col_B)];
+		bb[threadIdx.y][threadIdx.x] = B[N * (m * 16 + threadIdx.y) + (col_B)];
 
 		__syncthreads();
 
@@ -110,15 +95,12 @@ __global__ void mmm_implementation4(float* C, float* A, float* B) {
 		}
 
 		__syncthreads();
+
 	}
+
 	C[N * row_A + col_B] = tmp;
+
 }
-
-
-
-
-
-
 
 
 
@@ -132,31 +114,26 @@ int main()
 	cudaEventCreate(&stop);
 	float elapsed_time;
 
-	int devId = 0;
-	cudaDeviceProp prop;
-	cudaGetDeviceProperties(&prop, devId);
-	printf("\n Device: %s \n", prop.name);
-
 	MMM_init(); //initialize host arrays
 
 	float* C_d, * A_d, * B_d; //pointers to device arrays
 
-//---------------------------create GPU arrays------------------------------------------
-	cudaStatus = cudaMalloc((void**)&C_d, N * N * sizeof(float));//allocate memory dynamically 
+	//---------------------------create GPU arrays------------------------------------------
+	cudaStatus = cudaMalloc((void**)&C_d, N * N * sizeof(float));//allocate memory dynamically
 	if (cudaStatus != cudaSuccess) {//if the GPU memory asked is not available
 		printf("\nCudaMalloc failed");
 		cudaFree(C_d);
 		return -1;//returns unsuccessfully
 	}
 
-	cudaStatus = cudaMalloc((void**)&A_d, N * N * sizeof(float));//allocate memory dynamically 
+	cudaStatus = cudaMalloc((void**)&A_d, N * N * sizeof(float));//allocate memory dynamically
 	if (cudaStatus != cudaSuccess) {//if the GPU memory asked is not available
 		printf("\nCudaMalloc failed");
 		cudaFree(C_d); cudaFree(A_d);
 		return -1;//returns unsuccessfully
 	}
 
-	cudaStatus = cudaMalloc((void**)&B_d, N * N * sizeof(float));//allocate memory dynamically 
+	cudaStatus = cudaMalloc((void**)&B_d, N * N * sizeof(float));//allocate memory dynamically
 	if (cudaStatus != cudaSuccess) {//if the GPU memory asked is not available
 		printf("\nCudaMalloc failed");
 		cudaFree(C_d); cudaFree(A_d); cudaFree(B_d);
@@ -185,15 +162,14 @@ int main()
 
 	for (int it = 0; it < TIMES_TO_RUN; it++) {
 
-		//dim3 dimBlock(TILE, TILE, 1);
-		//dim3 dimGrid((N + TILE - 1) / TILE, (N + TILE - 1) / TILE, 1);
-		//mmm_ver1 << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
-		//mmm_tiled << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
-		//mmm_sw_pipeline <<< dimGrid, dimBlock >>> (C_d, A_d, B_d);
+		//dim3 dimBlock(1, 1, 1);
+		//dim3 dimGrid(1, 1, 1);
 
-		dim3 dimBlock(TILE, TILE, 1);
-		dim3 dimGrid((N + TILE_x2 - 1) / (TILE_x2), (N + TILE_x2 - 1) / (TILE_x2), 1);
-		mmm_implementation1 << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
+		dim3 dimBlock(16, 16, 1);
+		dim3 dimGrid(N / 16, N / 16, 1);
+		//mmm_ver1 << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
+		mmm_tiled << <dimGrid, dimBlock >> > (C_d, A_d, B_d);
+
 
 	}
 
@@ -226,7 +202,7 @@ int main()
 
 	//MMM_default();
 
-#ifdef CHECK_OUTPUT 
+#ifdef CHECK_OUTPUT
 	if (Compare_MMM() != 0)
 		printf("\n---------WRONG OUTPUT---------------\n");
 	else
@@ -282,15 +258,14 @@ unsigned short int equal(float const a, float const b) {
 		return 1;
 }
 
-
 int Compare_MMM() {
 
 	float tmp;
 	int i, j, k;
 
-	//optimize the following, otherwise it takes too long...however, to allow VS to use the \pragmas you must go 
-		//in project  properties and enable that (look at the lab session document for more info)
-#pragma omp parallel 
+	//optimize the following, otherwise it takes too long...however, to allow VS to use the \pragmas you must go
+	//in project  properties and enable that (look at the lab session document for more info)
+#pragma omp parallel
 	{
 #pragma omp for private(i, j, k, tmp)
 		for (i = 0; i < N; i++) {
@@ -313,8 +288,3 @@ int Compare_MMM() {
 			}
 	return 0;
 }
-
-
-
-
-
---
+++ Report/Question 2/Question2_Imp1_2/Question2_Imp1_2/Question2_Imp1_2.cpp
@@ -0,0 +1,161 @@
+// Question2Imp1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/* 
+To run:
+go into folder:
+cd .\Question2_Imp1_2\
+compile using:
+cl -O2 Question2_Imp1_2.cpp -openmp:experimental
+run using
+.\Question2_Imp1_2.exe
+*/
+#include <Windows.h>
+#include <stdio.h>
+#include <time.h>
+#include <pmmintrin.h>
+#include <process.h>
+#include <chrono>
+#include <iostream>
+#include <immintrin.h>
+
+#include <omp.h>
+
+#define BILLION 1000000000
+#define TIMES 10000
+
+#define N 64
+
+#define ARITHMETICAL_OPS (5*N)*N*N
+
+
+
+void imp_1();
+void imp_2();
+void MMM_init();
+
+__declspec(align(64)) float C[N * N], test[N * N], A[N * N], B[N * N]; //square matrixes are considered only, stored as 1d arrays
+
+
+//__declspec(align(64)) float  X[N], Y[N];
+//__declspec(align(64)) float A[N][N], Atr[N * N];
+
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+    double my_flops;
+
+    MMM_init();
+
+    //the following command pins the current process to the 1st core
+     //otherwise, the OS tongles this process between different cores
+    //BOOL success = SetProcessAffinityMask(GetCurrentProcess(), 1);
+    //if (success == 0) {
+    //    //cout << "SetProcessAffinityMask failed" << endl;
+    //    printf("\nSetProcessAffinityMask failed\n");
+    //    system("pause");
+    //    return -1;
+    //}
+
+    //define the timers measuring execution time
+    clock_t start_1, end_1; //ignore this for  now
+    start_1 = clock();
+
+    for (int t = 0; t < TIMES; t++)
+    {
+        //imp_1();
+        imp_2();
+    }
+    end_1 = clock(); //end the timer
+    printf("elapsed time = %f seconds\n", (float)(end_1 - start_1) / CLOCKS_PER_SEC);
+    printf("\n %d", N);
+    printf("\n start: %f", (float)start_1);
+    printf("\n end: %f", (float)end_1);
+    printf("\n TIMES: %f", (float)TIMES);
+    printf("\n OPS: %d", ARITHMETICAL_OPS);
+
+
+    //printf("\n The first and last values are %f %f\n", Y[0], Y[N - 1]);
+
+
+    my_flops = (double)(TIMES * (double)((ARITHMETICAL_OPS) / ((end_1) / CLOCKS_PER_SEC)));
+    printf("\n%f GigaFLOPS achieved\n", my_flops / BILLION);
+
+    return 0;
+}
+
+void imp_1() {
+    //implementation #1
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            for (int k = 0; k < N; k++)
+                C[N * i + j] += A[N * i + k] * B[N * k + j];
+}
+
+void imp_2() {
+    //implementation #2
+    //printf("\nimplementation 2");
+
+    //__declspec(align(64)) float C[N * N], A[N * N], B[N * N];
+
+    float tmp;
+    int i, j, k;
+
+#pragma omp parallel  
+
+    {
+
+#pragma omp for private(i, j, k, tmp)
+
+        for (i = 0; i < N; i++) {
+
+            for (j = 0; j < N; j++) {
+
+                tmp = 0.0;
+
+#pragma omp simd reduction(+:tmp) aligned(C,A,B:64)
+
+                for (k = 0; k < N; k++) {
+
+                    tmp += A[N * i + k] * B[N * k + j];
+
+                }
+
+                C[N * i + j] = tmp;
+
+            }
+
+        }
+
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started:
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+void MMM_init() {
+
+    float e = 0.1234, p = 0.7264, r = 0.11;
+
+    //MMM
+    for (unsigned int i = 0; i < N; i++) { //printf("\n");
+        for (unsigned int j = 0; j < N; j++) {
+            C[N * i + j] = 0.0;
+            test[N * i + j] = 0.0;
+            A[N * i + j] = (j % 9) + p; //printf(" %3.1f",A[i][j]);
+            B[N * i + j] = (j % 7) - p; //printf(" %3.1f",B[i][j]);
+        }
+    }
+
+
+}
